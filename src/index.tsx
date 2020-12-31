6078fff3233a92760cf0ad26f48a73da522a3d98
Mun4kin <mun4kin@MacBook-Pro-Mun4kin.local>
2020-12-31 12:41:33 +0300

    full version

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,63 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import intercept from './_utils/interceptor';
+import { Provider } from 'react-redux';
+
+import { HashRouter } from 'react-router-dom';
+import cssVars from 'css-vars-ponyfill';
+import variables from './_utils/colors.json';
+import { store } from './_store';
+
+/** Подключаем цвета для IE */
+cssVars({
+  watch: true,
+  include: 'style,link[rel="stylesheet"]:not([href*="//"])',
+  variables
+});
+/** Подключаем интерцептор */
+intercept();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Root= () => {
+  return <Provider store={store}>
+    <HashRouter>
+      <App/>
+    </HashRouter>
+  </Provider>
+}
+
+
+export default Root
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+//
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+//
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
--- webpack.config.js
+++ webpack.config.js
@@ -30,25 +30,52 @@ module.exports = {
       },
       {
         test: /\.css|\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader','resolve-url-loader','sass-loader']
+      },
+
+      {
+        test: /\.(svg|png|jpg|jpeg|gif|mp3)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: 'assets/[name].[ext]'
+          }
+        }]
+
       },
       {
-        test: /\.svg$/,
-        use: ['@svgr/webpack','url-loader'],
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        options: {
+          name: 'assets/fonts/[name].[ext]'
+        }
       },
+      {
+        test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/fonts/[name].[ext]'
+        }
+      }
     ]
   },
   node: {
     fs: 'empty'
+  },
+  stats:{
+    errorDetails: true, //this does show errors
+    // colors: false,
+    modules: true,
+    reasons: true
   },
 
   plugins: [
     new CleanWebpackPlugin({
       cleanAfterEveryBuildPatterns: ['dist']
     }),
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    }),
+    // new webpack.optimize.LimitChunkCountPlugin({
+    //   maxChunks: 1
+    // }),
     new webpack.PrefetchPlugin('react')
   ],
   devtool: 'source-map',
