ec095c5991e26ed3f0531be501f9b0961a5374cb
michaeladze <michael.kutateladze@yandex.ru>
2021-01-12 10:42:55 +0300

    feat: [master] hash

--- _worktime/src/index_singleSpa.tsx
+++ _worktime/src/index_singleSpa.tsx
@@ -24,7 +24,7 @@ intercept();
 const Root = () => {
   return (
     <Provider store={store}>
-      <Router basename='/#/worktime'>
+      <Router basename='#/worktime'>
         <App />
       </Router>
     </Provider>
