f4b9f54eb5494cfa5e0e9bf0223e739ea54e3e12
michaeladze <michael.kutateladze@yandex.ru>
2021-01-15 15:03:29 +0300

    feat: [master] Test webpack dotenv

--- _worktime/webpack.config.js
+++ _worktime/webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-require('dotenv').config()
+const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
 
 module.exports = {
-  entry: [ 'src/singleSpaEntry.tsx' ],
+  entry: ['src/singleSpaEntry.tsx'],
   output: {
     library: 'single-spa-worktime',
     libraryTarget: 'umd',
@@ -13,17 +13,25 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
-    extensions: [ '.ts', '.tsx', '.js' ],
-    modules: [ __dirname, 'node_modules' ]
+    extensions: ['.ts', '.tsx', '.js'],
+    modules: [__dirname, 'node_modules']
   },
   module: {
     rules: [
-      { test: /\.ts$/, exclude: [ path.resolve(__dirname, 'node_modules') ], loader: 'awesome-typescript-loader' },
-      { test: /\.tsx$/, exclude: [ path.resolve(__dirname, 'node_modules') ], loader: 'awesome-typescript-loader' },
+      {
+        test: /\.ts$/,
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.tsx$/,
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        loader: 'awesome-typescript-loader'
+      },
       {
         test: /\.js?$/,
-        exclude: [ path.resolve(__dirname, 'node_modules') ],
-        loader: [ 'babel-loader', 'eslint-loader' ]
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        loader: ['babel-loader', 'eslint-loader']
       },
 
       {
@@ -32,34 +40,34 @@ module.exports = {
       },
       {
         test: /\.css|\.scss$/,
-        use: [ 'style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader' ]
+        use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/,
-        use: [ {
+        use: [{
           loader: 'file-loader',
           options: {
             name: 'assets/img/[name].[ext]'
           }
-        } ]
+        }]
       },
       {
         test: /\.(svg)$/,
-        use: [ {
+        use: [{
           loader: 'file-loader',
           options: {
             name: 'assets/svg/[name].[ext]'
           }
-        } ]
+        }]
       },
       {
         test: /\.(mp3)$/,
-        use: [ {
+        use: [{
           loader: 'file-loader',
           options: {
             name: 'assets/mp3/[name].[ext]'
           }
-        } ]
+        }]
 
       },
       {
@@ -90,13 +98,14 @@ module.exports = {
 
   plugins: [
     new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns: [ 'dist' ]
+      cleanAfterEveryBuildPatterns: ['dist']
     }),
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
     }),
     new webpack.PrefetchPlugin('react'),
     new webpack.DefinePlugin({
+      'process.env': dotenv.parsed,
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
     new webpack.EnvironmentPlugin(['NODE_ENV'])
