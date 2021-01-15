bef813d36e3280575553cce72988bee7a639565a
michaeladze <michael.kutateladze@yandex.ru>
2021-01-15 11:42:48 +0300

    feat: [master] test

--- _worktime/src/App.tsx
+++ _worktime/src/App.tsx
@@ -15,77 +15,66 @@ import cloud5 from './assets/img/cloud5.png';
 import { getTeamPending } from './_store/actions/team.actions';
 import { getHistoryPending, getTasksPending } from './_store/actions/history.actions';
 import { IStore } from './_store';
-import { BehaviorSubject, interval } from 'rxjs';
+import { interval } from 'rxjs';
 import { takeWhile } from 'rxjs/operators';
 // @ts-ignore
 import sound from './assets/mp3/1.mp3';
 import ReactAudioPlayer from 'react-audio-player';
-import Notifications from './components/atoms/Notifications';
-import { INotification } from './components/atoms/Notifications/Notifications';
 
 const App = (props: any) => {
-  // // -------------------------------------------------------------------------------------------------------------------
-  // const dispatch = useDispatch();
-  // // при входе сразу запрашиваем кто зашел и справочники
-  // const [ready, setReady] = useState(false);
-  // const stop = useSelector((state: IStore) => state.stopApp.stopApplication);
-  //
-  // useEffect(() => {
-  //   if (props.user) {
-  //     dispatch(userInfoSuccess(props.user));
-  //   } else {
-  //     dispatch(userInfoPending());
-  //   }
-  // }, [props.user]);
-  //
-  // useEffect(() => {
-  //   if (!stop) {
-  //     dispatch(getTeamPending());
-  //     dispatch(getHistoryPending());
-  //     dispatch(getTasksPending());
-  //     //запрашиваем раз в 20 сек изменения
-  //   }
-  //   const tmp = interval(20000)
-  //     .pipe(takeWhile((_) => !stop))
-  //     .subscribe(() => {
-  //       dispatch(getTasksPending());
-  //     });
-  //   return () => {
-  //     tmp.unsubscribe();
-  //   };
-  // }, [stop]);
-
-  const [state, setState] = useState(false);
+  // -------------------------------------------------------------------------------------------------------------------
+  const dispatch = useDispatch();
+  // при входе сразу запрашиваем кто зашел и справочники
+  const [ready, setReady] = useState(false);
+  const stop = useSelector((state: IStore) => state.stopApp.stopApplication);
 
   useEffect(() => {
-    console.log(1);
-  }, [state]);
+    if (props.user) {
+      dispatch(userInfoSuccess(props.user));
+    } else {
+      dispatch(userInfoPending());
+    }
+  }, [props.user]);
 
-  const selector = useSelector((store: IStore) => store);
+  useEffect(() => {
+    if (!stop) {
+      dispatch(getTeamPending());
+      dispatch(getHistoryPending());
+      dispatch(getTasksPending());
+      //запрашиваем раз в 20 сек изменения
+    }
+    const tmp = interval(20000)
+      .pipe(takeWhile((_) => !stop))
+      .subscribe(() => {
+        dispatch(getTasksPending());
+      });
+    return () => {
+      tmp.unsubscribe();
+    };
+  }, [stop]);
 
   return (
     <>
-      <h1>Simple App Worktime </h1>
-      {/*{ready && (*/}
-      {/*  <div className='banner__wrapper'>*/}
-      {/*    <ReactAudioPlayer src={sound} volume={0.1} autoPlay={true} />*/}
-      {/*    <div className='banner'>*/}
-      {/*      <div className='clouds'>*/}
-      {/*        <img alt='' className='id1' src={cloud1} />*/}
-      {/*        <img alt='' className='id2' src={cloud2} />*/}
-      {/*        <img alt='' className='id3' src={cloud3} />*/}
-      {/*        <img alt='' className='id4' src={cloud4} />*/}
-      {/*        <img alt='' className='id5' src={cloud5} />*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*)}*/}
-      {/*<div className={`app ${ready ? 'winter' : ''}`}>*/}
-      {/*  <AppHeader setReady={setReady} />*/}
-      {/*  <Router routes={routes} />*/}
-      {/*  /!*<Notifications />*!/*/}
-      {/*  <PopupMaker />*/}
-      {/*</div>*/}
+      {ready && (
+        <div className='banner__wrapper'>
+          <ReactAudioPlayer src={sound} volume={0.1} autoPlay={true} />
+          <div className='banner'>
+            <div className='clouds'>
+              <img alt='' className='id1' src={cloud1} />
+              <img alt='' className='id2' src={cloud2} />
+              <img alt='' className='id3' src={cloud3} />
+              <img alt='' className='id4' src={cloud4} />
+              <img alt='' className='id5' src={cloud5} />
+            </div>
+          </div>
+        </div>
+      )}
+      <div className={`app ${ready ? 'winter' : ''}`}>
+        <AppHeader setReady={setReady} />
+        <Router routes={routes} />
+        {/*<Notifications />*/}
+        {/*<PopupMaker />*/}
+      </div>
     </>
   );
 };
